edf717fa85cba603788daf71ae36e885d86cd8bf
AlondraCordoba <alondracordoba0720@gmail.com>
2022-07-28 20:05:32 -0500

    feat: project initialized

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const routes = require('./routes/index');
+const mongoose = require('mongoose');
+const mongoClient = require('mongodb').MongoClient;
+const config = require('./config/config');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/app', routes);
+
+mongoose.connect(config.NODE_SERVER).then((data) => {
+    console.log("Connected to Database");
+}).catch((error) => {
+    console.log(error);
+});
+
+app.listen(process.env.PORT || 5000);

d415e85509e21e61e8753b417b85d080eee4096b
ErikUTA <erik4tohj@gmail.com>
2022-08-11 11:51:27 -0500

    feat: created controllers for expenses and login

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const routes = require('./routes/index');
 const mongoose = require('mongoose');
 const mongoClient = require('mongodb').MongoClient;
 const config = require('./config/config');
+const auth = require('./routes/auth');
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/app', routes);
+app.get("/auth", auth, (req, res) => {
+    res.json({ message: "You are authorized to access me" });
+});
 
 mongoose.connect(config.NODE_SERVER).then((data) => {
     console.log("Connected to Database");
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const router = express.Router();
 const Gastos = require('../models/gastos');
 const Categorias = require('../models/categorias');
+const Users = require('../models/user');
+const bycrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// Categorias
 
 router.get('/categorias', async (req, res) => {
     try{
@@ -42,7 +47,7 @@ router.post('/post-category', async (req, res) => {
             res.json(result);
         }
     }catch(error){
-        res.estatus(500).send(error);
+        res.status(500).send(error);
     }
 });
 
@@ -57,7 +62,77 @@ router.post('/post-gasto', async (req, res) => {
         const result = await modelg.save();
         res.json(result);        
     }catch(error){
-        res.estatus(500).send(error);
+        res.status(500).send(error);
+    }
+});
+
+// Autenticacion
+
+router.get("/auth", async (req, res, next) => {
+    try {
+      const token = await req.headers.authorization.split(" ")[1];
+      const decodedToken = await jwt.verify(token, "RANDOM-TOKEN");
+      const user = await decodedToken;
+      request.user = user;
+      next();    
+    } catch (error) {
+      response.status(401).json({
+        error: new Error("Invalid request!"),
+      });
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const find = await Users.findOne({ email: req.body.email });
+        if(find) {
+            const result = bycrypt.compareSync(req.body.password, find.password);
+            if(result == false){
+                res.status(401).send("La contrasenia es incorrecta")
+            }else{
+                const token = jwt.sign(
+                    {
+                        user: req.body.email,
+                        password: req.body.password,                 
+                    },
+                    "RANDOM-TOKEN",
+                    { expiresIn: "24h" }
+                );
+    
+                res.status(200).send({
+                    message: "Bienvenido al sistema",
+                    email: req.body.email,
+                    token: token,
+                    id: find && find._id
+                });
+            }
+        }else{
+            res.status(401).send("No existe el usuario");
+        } 
+    } catch(error) {
+        res.status(500).send(error);
+    }
+});
+
+router.post('/register', async (req, res) => {
+    try {          
+        var user = await Users.findOne({ email: req.body.email });
+        if(user){
+            res.status(400).send("El usuario ya existe");
+        }else{
+            if(req.body.password === req.body.confirmPassword){
+                const modelo = new Users({
+                    email: req.body.email,
+                    password: bycrypt.hashSync(req.body.password, 10),
+                });
+                await modelo.save();
+                res.send(modelo);
+            }else{
+                res.status(400).send("Las contrasenias no coinciden");
+            }
+        } 
+    } catch(error) {
+        res.status(500).send(error);
     }
 });
 
